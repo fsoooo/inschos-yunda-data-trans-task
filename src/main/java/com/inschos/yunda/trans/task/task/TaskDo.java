3849f0ae4afd2597986f10b3b85aa61e225e7424
meiming <meiming@bigwei.com>
2018-07-15 20:56:11 +0800

    m

--- src/main/java/com/inschos/yunda/trans/task/task/TaskDo.java
+++ src/main/java/com/inschos/yunda/trans/task/task/TaskDo.java
@@ -47,7 +47,7 @@ public class TaskDo {
 
     private final int limit = 1000;
 
-    @Scheduled(cron = "1 1 21 15 7 2018")
+    @Scheduled(cron = "1 1 21 15 7 *")
     public void cronOnce(){
         run();
     }
